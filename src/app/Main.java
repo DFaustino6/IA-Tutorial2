ed875441cc63d910a6bf2060b52d26f1305d1cc9
DFaustino6 <a61171@ualg.pt>
2021-01-07 22:19:52 +0000

    starting

---
+++ src/app/Main.java
@@ -0,0 +1,19 @@
+
+package app;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+       Random generator = new Random(0); 
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       double d;
+
+       DecimalFormatSymbols unusualSymbols = new DecimalFormatSymbols();
+       unusualSymbols.setDecimalSeparators('.');
+       DecimalFormat df = new DecimalFormat("0.00",unusualSymbols);
+
+       for(int i = 0; i)
+
+    }
+}
