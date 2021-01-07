b205d5f113b6eec294e3db761edde584b07d53c5
DFaustino6 <a61171@ualg.pt>
2021-01-07 23:59:58 +0000

    G done

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,27 +5,23 @@ import java.util.*;
 
 public class Main {
     public static void main(String [] args) {
-        Random generator = new Random(0); 
+        //Random generator = new Random(0); 
         Scanner sc = new Scanner(System.in);    
 
-        int n = sc.nextInt();//size of the population
-        int l = sc.nextInt();//chromosome length    
-        double d;   
+        //int n = sc.nextInt();//size of the population
+        //int l = sc.nextInt();//chromosome length    
+        //double d;   
 
         //DecimalFormatSymbols unusualSymbols = new DecimalFormatSymbols();
         //unusualSymbols.setDecimalSeparator('.');
         //DecimalFormat df = new DecimalFormat("0.00",unusualSymbols);
 
-        
-        for (int i = 0; i < n; i++) {
-            StringBuilder s = new StringBuilder();
-            for(int j = 0; j < l; j++){
-                d = generator.nextDouble();       
-                s.append(Math.round(d));
-            }
-           System.out.println(s);
+        String s = sc.next();
+        int n = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='1') n++;
         }
-
+        System.out.println(n);
         sc.close();
 
     }
