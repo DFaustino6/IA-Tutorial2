c2de5190d33952416bf418dd2c0868ba352be3a8
DFaustino6 <a61171@ualg.pt>
2021-01-07 22:22:11 +0000

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,6 @@
 
 package app;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
@@ -13,7 +14,12 @@ public class Main {
        unusualSymbols.setDecimalSeparators('.');
        DecimalFormat df = new DecimalFormat("0.00",unusualSymbols);
 
-       for(int i = 0; i)
+       for (int i = 0; i < args.length; i++) {
+           d = generator.nextDouble();
+           System.out.println(df.format(d));
+       }
+
+       sc.close();
 
     }
 }
